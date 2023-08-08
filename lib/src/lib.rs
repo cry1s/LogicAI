f5bc21aaacc60f4131aaeffe1cb7da348bf86ad4
cry1s <cry1s@ya.ru>
2023-08-09 01:01:56 +0300

    a bit of nothing

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde_json::Value;
+
+pub struct Node {
+    parameter: Value
+}
+
+pub struct Edge {
+    rule: String,
+    start_nodes: Vec<Node>,
+    end_nodes: Vec<Node>
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests;
